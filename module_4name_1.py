ff950d2310e8b585fd3ee6f0525d6cacbd497a38
Bonick32 <Bonick3244@gmail.com>
2024-12-17 03:52:58 +0300

    first

--- module_4name_1.py
+++ module_4name_1.py
@@ -1,8 +1,8 @@
 def test_function():
     def inner_function():
-        return print(f'Я в области видимости функции test_function')
+        return print('Я в области видимости функции test_function')
 
     inner_function()
 
 test_function()
-#inner_function()
+# inner_function() # происходит ошибка, так как эта функция вложена в другую функцию
