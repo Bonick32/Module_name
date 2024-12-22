da2dad0c4c2ec8666cb0b6ddb663b39126164c84
Bonick32 <Bonick3244@gmail.com>
2024-12-22 14:25:03 +0300

    update

--- package1/module_4name_2.py
+++ package1/module_4name_2.py
@@ -32,77 +32,77 @@
 import names
 from time import time
 
-# def decor(func,*args):
-#     ls = args[0]
-#     print('получил', ls)
-#     start = time()
-#     func(*args)
-#     stop = time()
-#     print('отдал', ls)
-#     print('время', stop-start)
-#
-# #0.2
-# def bubble_sort(ls, reverse = False):
-#     compare = (lambda x,y : x > y) if not reverse else (lambda x,y : x < y)
-#     swapped = True
-#     while swapped:
-#         swapped = False
-#         for i in range(len(ls)-1):
-#             if compare(ls[i], ls[i+1]):
-#                 ls[i], ls[i + 1] = ls[i+1], ls[i]
-#                 swapped = True
-#
-#
-# # 0.03
-# def selection_sort(ls):
-#     for i in range(len(ls)): # i - кол уже проверенных
-#         low = i
-#         for j in range(i+1,len(ls)):
-#             if ls[low] > ls[j]:
-#                 low = j
-#         ls[low],ls[i] = ls[i],ls[low]
-#
-# def insertion_sort(ls):
-#     for i in range(1, len(ls)):
-#         item = ls[i]
-#         j = i - 1
-#         while j >= 0 and ls[j] > item:
-#             ls[j + 1] = ls[j]
-#             j -= 1
-#         ls[j + 1] = item
-#
-# # 0.001
-# def quick_sort(ls):
-#     if len(ls) <= 1:
-#         return ls
-#     el = ls[len(ls)//2]
-#     l = [i for i in ls if i< el]
-#     m = [i for i in ls if i == el]
-#     r = [i for i in ls if i > el]
-#
-#
-#     return quick_sort(l) + m +  quick_sort(r)
-#
-# def sort_(ls):
-#     ls = sorted(ls)
-#     return print(ls)
-
-
-
-
-# ls = [names.get_first_name() for i in range(10000)]
-
-def ls_(name):
+def decor(func,*args):
+    ls = args[0]
+    print('получил', ls)
     start = time()
-    name =  [names.get_first_name() for i in range(10000)]
+    func(*args)
     stop = time()
-    print('отдал', name)
-    print('время', stop - start)
+    print('отдал', ls)
+    print('время', stop-start)
 
-name = []
-ls_(name)
+#0.2
+def bubble_sort(ls, reverse = False):
+    compare = (lambda x,y : x > y) if not reverse else (lambda x,y : x < y)
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(ls)-1):
+            if compare(ls[i], ls[i+1]):
+                ls[i], ls[i + 1] = ls[i+1], ls[i]
+                swapped = True
 
-# decor(sort_, ls)
+
+# 0.03
+def selection_sort(ls):
+    for i in range(len(ls)): # i - кол уже проверенных
+        low = i
+        for j in range(i+1,len(ls)):
+            if ls[low] > ls[j]:
+                low = j
+        ls[low],ls[i] = ls[i],ls[low]
+
+def insertion_sort(ls):
+    for i in range(1, len(ls)):
+        item = ls[i]
+        j = i - 1
+        while j >= 0 and ls[j] > item:
+            ls[j + 1] = ls[j]
+            j -= 1
+        ls[j + 1] = item
+
+# 0.001
+def quick_sort(ls):
+    if len(ls) <= 1:
+        return ls
+    el = ls[len(ls)//2]
+    l = [i for i in ls if i< el]
+    m = [i for i in ls if i == el]
+    r = [i for i in ls if i > el]
+
+
+    return quick_sort(l) + m +  quick_sort(r)
+
+def sort_(ls):
+    ls = sorted(ls)
+    return print(ls)
+
+
+
+
+ls = [names.get_first_name() for i in range(10000)]
+
+# def ls_(name): #проверил скорость создания списка имен
+#     start = time()
+#     name =  [names.get_first_name() for i in range(10000)]
+#     stop = time()
+#     print('отдал', name)
+#     print('время', stop - start)
+#
+# name = []
+# ls_(name)
+
+decor(sort_, ls)
 
 
 # print(ls)
