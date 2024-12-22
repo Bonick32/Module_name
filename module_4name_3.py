a105b324c41ca73b58fea8d02e92754ad7feb5dd
Bonick32 <Bonick3244@gmail.com>
2024-12-22 22:21:08 +0300

    update №2 Задача "Developer - не только разработчик"

--- module_4name_3.py
+++ module_4name_3.py
@@ -6,6 +6,21 @@ class House:
                                                       # input, что является строкой, необходимо перевести ее в число
     def go_to(self, new_floor):
         new_floor = int(new_floor)
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for i in range(new_floor):
+                print(i+1)
+
+# ниже немного расширил код для дз, как мне кажется более информативный при выводе в консоль)
+
+class House2:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = int(number_of_floors)
+
+    def go_to2(self, new_floor):
+        new_floor = int(new_floor)
         print(f'всего этажей в "{self.name}" - {self.number_of_floors}')
         if new_floor > self.number_of_floors or new_floor < 1:
             print(f'поднимаемся на этаж №{new_floor}:')
@@ -17,8 +32,9 @@ class House:
 
 h1 = House('ЖК Горский', '18')
 h2 = House('Домик в деревне', 2)
-h3 = House(input('Введите название здания: '), input('количество этажей: '))
 
 h1.go_to(5)
 h2.go_to('10')
-h3.go_to(input('на какой этаж подняться: '))
+
+h3 = House2(input('Введите название здания: '), input('количество этажей: '))
+h3.go_to2(input('на какой этаж подняться: '))
