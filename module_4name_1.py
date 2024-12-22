ffa53f95335ee64b8cfaa277176dff3bf2f55f7f
Bonick32 <Bonick3244@gmail.com>
2024-12-22 14:18:26 +0300

    Задача "Developer - не только разработчик"

--- module_4name_1.py
+++ module_4name_1.py
@@ -1,8 +1,25 @@
-def test_function():
-    def inner_function():
-        return print('Я в области видимости функции test_function')
+from random import randint
+# def test_function():
+#     def inner_function():
+#         return print('Я в области видимости функции test_function')
+#
+#     inner_function()
+#
+# test_function()
+# # inner_function() # происходит ошибка, так как эта функция вложена в другую функцию
 
-    inner_function()
 
-test_function()
-# inner_function() # происходит ошибка, так как эта функция вложена в другую функцию
+MAX_BUNCHES = 5
+MAX_BUNCHE_SIZE = 20
+
+_holder = {}
+_sorted_keys = None
+
+
+def put_stones():
+    """ расположить камни на игровой поверхности """
+    global _holder, _sorted_keys
+    _holder = {}
+    for i in range(1, MAX_BUNCHES + 1):
+        _holder[i] = randint(1, MAX_BUNCHE_SIZE)
+    _sorted_keys = sorted(_holder.keys())
---
+++ package1/module_4name_2.py
@@ -0,0 +1,110 @@
+# # sortfunc
+#
+# nums = [5, 6, 3, 2, 1, 4]
+#
+# def buble_sort(ls):
+#     swapped = True
+#     while swapped:
+#         swapped = False
+#         for i in range(len(ls) - 1):
+#             if ls[i] > ls[i+1]:
+#                 ls[i], ls[i+1] = ls[i+1], ls[i]
+#                 swapped = True
+#
+# # buble_sort(nums)
+# # print(nums)
+#
+# def selection_sort(ls):
+#     for i in range(len(ls)):
+#         lowest = i
+#         for j in range(i + 1, len(ls)):
+#             if ls[j] < ls[lowest]:
+#                 lowest = j
+#         ls[i], ls[lowest] = ls[lowest], ls[i]
+#
+# # print(nums)
+# # selection_sort(nums)
+# # print(nums)
+#
+
+
+
+import names
+from time import time
+
+# def decor(func,*args):
+#     ls = args[0]
+#     print('получил', ls)
+#     start = time()
+#     func(*args)
+#     stop = time()
+#     print('отдал', ls)
+#     print('время', stop-start)
+#
+# #0.2
+# def bubble_sort(ls, reverse = False):
+#     compare = (lambda x,y : x > y) if not reverse else (lambda x,y : x < y)
+#     swapped = True
+#     while swapped:
+#         swapped = False
+#         for i in range(len(ls)-1):
+#             if compare(ls[i], ls[i+1]):
+#                 ls[i], ls[i + 1] = ls[i+1], ls[i]
+#                 swapped = True
+#
+#
+# # 0.03
+# def selection_sort(ls):
+#     for i in range(len(ls)): # i - кол уже проверенных
+#         low = i
+#         for j in range(i+1,len(ls)):
+#             if ls[low] > ls[j]:
+#                 low = j
+#         ls[low],ls[i] = ls[i],ls[low]
+#
+# def insertion_sort(ls):
+#     for i in range(1, len(ls)):
+#         item = ls[i]
+#         j = i - 1
+#         while j >= 0 and ls[j] > item:
+#             ls[j + 1] = ls[j]
+#             j -= 1
+#         ls[j + 1] = item
+#
+# # 0.001
+# def quick_sort(ls):
+#     if len(ls) <= 1:
+#         return ls
+#     el = ls[len(ls)//2]
+#     l = [i for i in ls if i< el]
+#     m = [i for i in ls if i == el]
+#     r = [i for i in ls if i > el]
+#
+#
+#     return quick_sort(l) + m +  quick_sort(r)
+#
+# def sort_(ls):
+#     ls = sorted(ls)
+#     return print(ls)
+
+
+
+
+# ls = [names.get_first_name() for i in range(10000)]
+
+def ls_(name):
+    start = time()
+    name =  [names.get_first_name() for i in range(10000)]
+    stop = time()
+    print('отдал', name)
+    print('время', stop - start)
+
+name = []
+ls_(name)
+
+# decor(sort_, ls)
+
+
+# print(ls)
+# ls = quick_sort(ls)
+# print(ls)
