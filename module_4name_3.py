623d1782eacc1b4f52ff45bbc9c960d52a84f9e6
Bonick32 <Bonick3244@gmail.com>
2024-12-23 20:47:25 +0300

    update №3 Задача "Магические здания"

--- module_4name_3.py
+++ module_4name_3.py
@@ -1,9 +1,8 @@
 class House:
     def __init__(self, name, number_of_floors):
         self.name = name
-        self.number_of_floors = int(number_of_floors) # в тексте задания обратил внимание на уточнение "номер этажа(int)"
-                                                      # понимая, что здесь при практике будет значение, введенное пользователем через
-                                                      # input, что является строкой, необходимо перевести ее в число
+        self.number_of_floors = int(number_of_floors)
+
     def go_to(self, new_floor):
         new_floor = int(new_floor)
         if new_floor > self.number_of_floors or new_floor < 1:
@@ -12,29 +11,44 @@ class House:
             for i in range(new_floor):
                 print(i+1)
 
-# ниже немного расширил код для дз, как мне кажется более информативный при выводе в консоль)
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
-class House2:
-    def __init__(self, name, number_of_floors):
-        self.name = name
-        self.number_of_floors = int(number_of_floors)
+    def __len__(self):
+        return self.number_of_floors
 
-    def go_to2(self, new_floor):
-        new_floor = int(new_floor)
-        print(f'всего этажей в "{self.name}" - {self.number_of_floors}')
-        if new_floor > self.number_of_floors or new_floor < 1:
-            print(f'поднимаемся на этаж №{new_floor}:')
-            print('Такого этажа не существует')
-        else:
-            print(f'поднимаемся на этаж №{new_floor}:')
-            for i in range(new_floor):
-                print(i+1)
+h1 = House('ЖК Горский', 18)
+h2 = House('ЖК Акация', 20)
 
-h1 = House('ЖК Горский', '18')
-h2 = House('Домик в деревне', 2)
+print(h1)
+print(h2)
 
-h1.go_to(5)
-h2.go_to('10')
+print(len(h1))
+print(len(h2))
 
-h3 = House2(input('Введите название здания: '), input('количество этажей: '))
-h3.go_to2(input('на какой этаж подняться: '))
+# h1.go_to(5)
+# h2.go_to('121')
+
+# class House2:
+#     def __init__(self, name, number_of_floors):
+#         self.name = name
+#         self.number_of_floors = int(number_of_floors)
+#
+#     def go_to2(self, new_floor):
+#         new_floor = int(new_floor)
+#
+#         print(f'поднимаемся на этаж №{new_floor}:')
+#         if new_floor > self.number_of_floors or new_floor < 1:
+#             print('Такого этажа не существует')
+#         else:
+#             for i in range(new_floor):
+#                 print(i+1)
+#     def __str__(self):
+#         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+#
+#     def __len__(self):
+#         return self.number_of_floors
+#
+# h3 = House2('Sweet Home', 2)
+# print(h3)
+# h3.go_to2(3)
