fa6a227b3ac6f96c0583afc7f2ec774c4efedfe8
Bonick32 <Bonick3244@gmail.com>
2024-12-25 01:47:44 +0300

    update №4 Задача "Нужно больше этажей":

--- module_4name_3.py
+++ module_4name_3.py
@@ -17,18 +17,75 @@ class House:
     def __len__(self):
         return self.number_of_floors
 
-h1 = House('ЖК Горский', 18)
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        self.number_of_floors = self.number_of_floors + value
+        return self
+
+    def __radd__(self, value):
+        self.number_of_floors = self.number_of_floors + value
+        return self
+
+    def __iadd__(self, value):
+        self.number_of_floors += value
+        return self
+
+h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
 print(h1)
 print(h2)
 
-print(len(h1))
-print(len(h2))
+print(h1 == h2) # __eq__
 
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
+
+
+
+
+
+# h1 = House('ЖК Горский', 18)
+# h2 = House('ЖК Акация', 20)
+#
+# print(h1)
+# print(h2)
+#
+# print(len(h1))
+# print(len(h2))
 # h1.go_to(5)
 # h2.go_to('121')
-
+#
 # class House2:
 #     def __init__(self, name, number_of_floors):
 #         self.name = name
